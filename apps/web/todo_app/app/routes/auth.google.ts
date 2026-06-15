f47fbea429883f6e583fed3d2bf696455a1a828e
Susankemigisa <kemigisa32@gmail.com>
2026-06-15 19:12:02 +0300

    Remove duplicate frontend and keep monorepo structure

--- apps/web/todo_app/app/routes/auth.google.ts
+++
@@ -1,6 +0,0 @@
-import { redirect } from "react-router";
-import { getGoogleAuthUrl } from "../services/google.server";
-
-export async function loader() {
-  return redirect(getGoogleAuthUrl());
-}
